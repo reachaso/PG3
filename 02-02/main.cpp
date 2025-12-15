417bfcd3eca654c148481564ad57ab6cd67863b8
reachaso <reachaso040505@gmail.com>
2025-12-15 12:48:15 +0900

    2-2修正

--- 02-02/main.cpp
+++ 02-02/main.cpp
@@ -4,23 +4,25 @@
 int main() {
   // 何時間働くか
   int workTime = 9;
-
   // 一般的な賃金体系(時給1226円)
   int hourly_wage = 1226;
-
-  // 再帰的な賃金体系 (最初 : 100円 2回目以降 : 前の1時間で貰った時給 * 2 -
-  // 50円)
-  int recursion = 100;
+  // 再帰的な賃金体系 (最初 : 100円 2回目以降 : 前の1時間で貰った時給 * 2 - 50円)
+  int recursionHourly = 100;
+  // 合計金額を格納する変数
+  int normalTotal = 0;
+  int recursionTotal = 0;
 
   for (int i = 1; i <= workTime; ++i) {
+    normalTotal += hourly_wage;
+    recursionTotal += recursionHourly;
+
     printf("\n%dhour\n", i);
-    printf("hourly_wage : %d\n", hourly_wage * i);
-    if (i == 1) {
-      printf("recursion : %d\n", recursion);
-    } else {
-      recursion = recursion * 2 - 50; // 再帰的な計算を修正
-      printf("recursion : %d\n", recursion);
-    }
+    printf("hourly_wage (per hour) : %d\n", hourly_wage);
+    printf("recursion (per hour) : %d\n", recursionHourly);
+    printf("hourly_wage (total) : %d\n", normalTotal);
+    printf("recursion (total) : %d\n", recursionTotal);
+
+    recursionHourly = recursionHourly * 2 - 50; // 再帰的な計算を修正
   }
 
   return 0;
