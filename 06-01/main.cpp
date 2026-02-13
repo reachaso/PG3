8cdc73d377f8b937b7d636e8e2e55a15f552e607
reachaso <reachaso040505@gmail.com>
2026-02-13 13:11:35 +0900

    修正

--- 06-01/main.cpp
+++ 06-01/main.cpp
@@ -1,13 +1,39 @@
+#include <condition_variable>
+#include <cstdio>
 #include <iostream>
+#include <mutex>
 #include <thread>
 
 using namespace std;
 
 int main() {
+  mutex mtx;
+  condition_variable cv;
+  int turn = 1;
 
-  thread t1([]() { printf("thread1\n"); });
-  thread t2([]() { printf("thread2\n"); });
-  thread t3([]() { printf("thread3\n"); });
+  thread t1([&]() {
+    unique_lock<mutex> lock(mtx);
+    cv.wait(lock, [&]() { return turn == 1; });
+    printf("thread1\n");
+    turn = 2;
+    cv.notify_all();
+  });
+
+  thread t2([&]() {
+    unique_lock<mutex> lock(mtx);
+    cv.wait(lock, [&]() { return turn == 2; });
+    printf("thread2\n");
+    turn = 3;
+    cv.notify_all();
+  });
+
+  thread t3([&]() {
+    unique_lock<mutex> lock(mtx);
+    cv.wait(lock, [&]() { return turn == 3; });
+    printf("thread3\n");
+    turn = 4;
+    cv.notify_all();
+  });
 
   t1.join();
   t2.join();
